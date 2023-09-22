b6ef4a8414716077058b13be76e3bd04953dfc85
habib <habiblawal@hotmail.co.uk>
2023-09-22 17:35:46 +0100

    WIP

--- src/main/java/io/openliberty/elph/cmd/ImportCommand.java
+++ src/main/java/io/openliberty/elph/cmd/ImportCommand.java
@@ -6,6 +6,10 @@ import picocli.CommandLine.Parameters;
 
 import java.util.List;
 
+import static picocli.CommandLine.Help.Ansi.Style.bold;
+import static picocli.CommandLine.Help.Ansi.Style.italic;
+import static picocli.CommandLine.Help.Ansi.Style.reset;
+
 @Command(name = ImportCommand.SUBCOMMAND_NAME, description = "Add project and its dependencies to Eclipse. ")
 class ImportCommand extends AbstractImportCommand implements Runnable {
     static final String SUBCOMMAND_NAME = "import";
@@ -19,6 +23,20 @@ class ImportCommand extends AbstractImportCommand implements Runnable {
     public void run() {
         addToHistory(patterns, includeUsers);
         var projects = findProjects(patterns.stream(), includeUsers);
+        eclipseImportCheckboxCheck();
         importDeps(projects);
     }
+
+    private void eclipseImportCheckboxCheck() {
+        System.out.println( bold.on() + "\nPlease press return after reading the following" + bold.off() + reset.on());
+
+        System.out.println("\nEnsure the following boxes are" + bold.on() + " UNTICKED \u274C" + bold.off() + reset.on());
+        System.out.println(italic.on() + "\t - Close newly imported projects upon deletion");
+        System.out.println("\t - Search for nested projects");
+        System.out.println("\t - Detect and configure project natures" + italic.off());
+
+        System.out.println("\nEnsure the following boxes are" + bold.on() + " TICKED \u2705" + bold.off() + reset.on());
+        System.out.println(italic.on() + "\t - Hide already open projects" + italic.off());
+    }
+
 }
