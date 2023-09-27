4f6f71e3302115cea47e6710a35fbf6d1bc5e780
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2023-09-26 18:27:34 -0700

    Invoke eclipse on Windows

--- src/main/java/io/openliberty/elph/util/OS.java
+++ src/main/java/io/openliberty/elph/util/OS.java
@@ -8,14 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum OS {
-    LINUX("eclipse") {
-        Stream<Object> getArgStream(Path eclipseHome, Path eclipseWorkspace, Path... projects) {
-            var exec = eclipseHome.resolve("eclipse");
-            return projects.length == 0 ?
-                    Stream.of(exec, "-data", eclipseWorkspace) :
-                    Stream.concat(Stream.of(exec), Stream.of(projects));
-        }
-    },
+    LINUX("eclipse"),
     MAC("Contents/MacOS/eclipse") {
         Stream<Object> getArgStream(Path eclipseHome, Path eclipseWorkspace, Path... projects) {
             var exec = eclipseHome;
@@ -24,19 +17,20 @@ public enum OS {
                     Stream.concat(Stream.of("open", "-a", exec), Stream.of(projects));
         }
     },
-    WINDOWS("eclipsec.exe") {
-        Stream<Object> getArgStream(Path eclipseHome, Path eclipseWorkspace, Path... projects) {
-            throw new UnsupportedOperationException("Please contact the maintainer to add support for your OS");
-        }
-    };
+    WINDOWS("eclipse.exe");
     public final Path pathToExecutable;
     OS(String exec) {this.pathToExecutable = Paths.get(exec);}
     public static OS current() {return valueOf(System.getProperty("os.name").toUpperCase().split(" ")[0]);}
     public static boolean is(OS os) { return current() == os; }
-    abstract Stream<Object> getArgStream(Path eclipseHome, Path eclipseWorkspace, Path...projects);
     public List<String> getEclipseCmd(Path eclipseHome, Path eclipseWorkspace, Path... projects) {
         return getArgStream(eclipseHome, eclipseWorkspace, projects)
                 .map(Objects::toString)
                 .toList();
+    }
+    Stream<Object> getArgStream(Path eclipseHome, Path eclipseWorkspace, Path... projects) {
+        var exec = eclipseHome.resolve(pathToExecutable);
+        return projects.length == 0 ?
+                Stream.of(exec, "-data", eclipseWorkspace) :
+                Stream.concat(Stream.of(exec), Stream.of(projects));
     }
 }
